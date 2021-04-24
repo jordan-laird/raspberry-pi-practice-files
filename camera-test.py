735e220ea6ba2d2aa19088996726dc7035d49b26
Jordan Laird <jordanmack.laird@gmail.com>
2021-04-23 23:44:07 -0500

    small changes

--- camera-test.py
+++ camera-test.py
@@ -2,5 +2,5 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-
-camera.capture('home/pi/Desktop/python-test-image.jpg')
+camera.rotation = 180
+camera.capture('home/pi/Documents/rasberry-pi-practice-files/python-test-image.jpg')
